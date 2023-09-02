1f18e5c61a5b168437f3496da2c945dce574a268
MHShihan <mahmodul.no.1@gmail.com>
2023-09-02 13:13:57 +0600

    Main part complete

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const loadCategory = async () => {
   );
   const data = await res.json();
   const categoryData = data.data;
-  //   console.log(categoryData);
+  console.log(categoryData);
   const categoryButtonContainer = document.getElementById(
     "dynamic-button-container"
   );
@@ -13,41 +13,66 @@ const loadCategory = async () => {
     div.innerHTML = `
       <button onclick ="handleLoadCard('${category.category_id}')" class="btn">${category.category}</button>
       `;
+
     categoryButtonContainer.appendChild(div);
   });
 };
 
+// Fetching Category Id
 const handleLoadCard = async (categoryID) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${categoryID}`
   );
   const data = await res.json();
+  // console.log(categoryID);
+
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   data.data.forEach((card) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <div>
-    <div class="mb-2 p-1">
-      <img class = "h-[200px] rounded-xl" src="${card?.thumbnail}" alt="" />
+    <div class="mb-2 p-1  ">
+      <img class = " h-[200px] w-auto rounded-xl" src="${
+        card?.thumbnail
+      }" alt="" />
     </div>
     <div class="flex gap-2 mt-4">
-      <img class = "rounded-full w-1/6 h-1/6" src="${card?.authors[0]?.profile_picture}" alt="" />
+      <img class = "rounded-full w-[40px] h-[40px] mt-4" src="${
+        card?.authors[0]?.profile_picture
+      }" alt="" />
       <div class="flex-1">
         <h4 class="text-base font-bold text-[#171717] mt-2">
-          Building a Winning UX Strategy Using the Kano Model
+          ${card?.title}
         </h4>
-        <p class="text-sm text-[#171717] font-medium mt-1">
-          <span>Awlad Hossain</span>
+        <p  class="text-sm text-[#171717] font-medium mt-2 mb-2 flex gap-4">
+          <span>${card?.authors[0]?.profile_name}</span> <span >${
+      card?.authors[0]?.verified
+        ? `<img src="images/fi_10629607.png" alt="" />`
+        : " "
+    } </span>
         </p>
-        <p class="text-sm text-[#171717] font-medium">91K views</p>
+        <p class="text-sm text-[#171717] font-medium">${
+          card?.others?.views
+        } views</p>
       </div>
     </div>
   </div>
     `;
+
     cardContainer.appendChild(div);
   });
-  console.log(data.data);
+
+  // console.log(data.data);
+  const handleDrawing = (categoryID) => {
+    const drawingBtn = document.getElementById("drawing-btn");
+    if (categoryID === "1005") {
+      drawingBtn.classList.remove("hidden");
+    } else {
+      drawingBtn.classList.add("hidden");
+    }
+  };
+  handleDrawing(categoryID);
 };
 
 loadCategory();
